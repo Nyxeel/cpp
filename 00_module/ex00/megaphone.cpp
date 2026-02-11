d5b4867ebce49c864d2463adb630752ce2cddb46
Patrick <no@mail.com>
2026-02-11 03:01:42 +0100

    FINISHED: ex00/

--- 00_module/ex00/megaphone.cpp
+++ 00_module/ex00/megaphone.cpp
@@ -6,13 +6,28 @@
 /*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 21:38:00 by pjelinek          #+#    #+#             */
-/*   Updated: 2026/02/10 22:07:39 by pjelinek         ###   ########.fr       */
+/*   Updated: 2026/02/11 02:59:57 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstring>
+
 int main(int ac, char **av)
 {
-	if (ac > 2)
-		std
+	if (ac > 1)
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (size_t j = 0; j < (std::strlen(av[i])); j++)
+			{
+				int c = std::toupper(av[i][j]);
+				std::cout << (char)c;
+			}
+			std::cout << std::endl;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }
