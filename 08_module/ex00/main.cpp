1f7e7293ecebd864d2b38801526df1ad7a298277
Nyxeel <nyxeel@protonmail.com>
2026-06-11 13:52:29 +0200

    Update progress

---
+++ 08_module/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 13:33:31 by pjelinek          #+#    #+#             */
+/*   Updated: 2026/06/11 13:49:08 by pjelinek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+#include <iterator>
+
+int	main(void)
+{
+	std::vector<int>	a;
+	std::list<int>		b;
+	std::deque<int>		c;
+
+	std::vector<int>::iterator it = a.begin();
+
+
+	return (0);
+}
